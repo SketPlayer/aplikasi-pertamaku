5708a0dd193a3dab6948a8c5c91a2ac272e57c46
SketPlayer <riohu137@gmail.com>
2024-10-22 21:30:14 +0700

    Deployment with Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env.VITE_SUBDOMAIN': JSON.stringify(process.env.VITE_SUBDOMAIN)
-  }
+  base: `/${process.env.VITE_SUBDOMAIN}/`,
 })
